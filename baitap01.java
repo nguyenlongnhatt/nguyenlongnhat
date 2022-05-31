36cfe7a07fd40cc11e11d3e6a4f1c043cfb9b9ab
nguyenlongnhatt <nhatn2444@gmail.com>
2022-06-01 06:28:56 +0700

    Create baitap01.java
    baitap01

---
+++ baitap01.java
@@ -0,0 +1,18 @@
+public class baitap01 {
+    public static void main(String[] args) {
+        int a=2;
+        int b=4;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(b-a);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(b/a);
+
+        int i=3;
+        int j=i++;
+        int k=++i;
+        System.out.println(j);
+        System.out.println(k);
+    }
+}
